a244c5d4becea2f6d8d49302dde9e4886ce64899
mathan26 <mathan.se@gmail.com>
2020-06-27 17:05:57 +0530

    Composite Pattern commit

---
+++ src/com/github/mathan/design_patterns/composite/Leaf.java
@@ -0,0 +1,25 @@
+package com.github.mathan.design_patterns.composite;
+
+public class Leaf extends Component {
+    public Leaf(String name) {
+        super(name);
+    }
+
+    @Override
+    void print(int level) {
+        for (int i = 0; i < level; i++) {
+            System.out.print("--");
+        }
+        System.out.println("left:" + name);
+    }
+
+    @Override
+    public void add(Component component) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void remove(Component component) {
+        throw new UnsupportedOperationException();
+    }
+}
